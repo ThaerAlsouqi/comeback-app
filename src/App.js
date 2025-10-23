a55bb00a6ff8d0a613e687bda6f320901bd3411d
Thaer Alsouqi <Thaerbelal7@gmail.com>
2025-10-23 18:41:47 +0300

    ...

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from "./logo.svg";
 import "./App.css";
+import MyButton from "./MyButton";
+const user = {
+  name: "John Doe",
+};
+const products = [
+  { id: 1, name: "Laptop", isFruit: false, price: 999 },
+  { id: 2, name: "Smartphone", isFruit: false, price: 699 },
+  { id: 3, name: "Tablet", isFruit: true, price: 499 },
+];
+const listItems = products.map((product) => (
+  <li
+    key={product.id}
+    style={{
+      color: product.isFruit ? "red" : "blue",
+    }}
+  >
+    {product.name}: ${product.price}
+  </li>
+));
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{user.name}</h1>
+      <ul>{listItems}</ul>
+      <MyButton />
+      <MyButton />
     </div>
   );
 }
---
+++ src/MyButton.js
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function MyButton() {
+  const [count, setCount] = useState(0);
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return <button onClick={handleClick}>Click Me {count} Times</button>;
+}
+export default MyButton;
